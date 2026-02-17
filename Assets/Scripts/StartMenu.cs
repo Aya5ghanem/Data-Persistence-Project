975ba6499d774d96db3f58c26d155ea3df85d0d5
Aya5ghanem <104633565+Aya5ghanem@users.noreply.github.com>
2026-02-17 13:10:17 +0200

    p1

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public TMP_InputField nameInput;
+
+    public void StartGame()
+    {
+        PlayerData.playerName = nameInput.text;
+        SceneManager.LoadScene("main"); // اسم مشهد اللعبة
+    }
+}
